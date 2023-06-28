603ac1b016d722098593bfaf2c1a1ed8a46779a2
Svyatoslav Matyuha <svyatoslavmatyuha@gmail.com>
2023-06-28 19:57:16 +0300

    feat: add carousel

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface MovieFeed {
+  title: string
+  poster: string
+  year: string
+  id: string
+  rate: string
+  link?: string
+}
+
+defineProps<MovieFeed>()
+</script>
+<template>
+  <div class="mr-8">
+    <img :src="poster" alt="movie poster" class="h-48 w-96 rounded-lg" />
+    <div class="flex mt-4 justify-between">
+      <h2 class="text-white text-xl">{{ title }}</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import Page from '../components/layout/Page.vue'
 import Tabs from '../components/Tabs.vue'
 import MainBanner from '../components/MainBanner.vue'
+import MovieFeed from '../components/MovieFeed.vue'
 import FlashNews from '../components/FlashNews.vue'
 import ContentRow from '../components/ContentRow.vue'
 import { tabsMock } from '../mocks/tabs'
@@ -24,6 +25,10 @@ const onTabChange = (tabId: string) => {
       <div class="w-full ml-10 max-w-md">
         <flash-news />
       </div>
+    </content-row>
+    <content-row class="flex-col">
+      <h1 class="text-2xl text-white font-bold">Special for you</h1>
+      <movie-feed></movie-feed>
     </content-row>
   </page>
 </template>
