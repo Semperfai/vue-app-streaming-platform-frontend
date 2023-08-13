112adbd3a64d0a4ce2072353cbd80198ae9d0422
Svyatoslav Matyuha <svyatoslavmatyuha@gmail.com>
2023-08-13 13:38:25 +0300

    feat: create time-control component

--- src/components/Player/VideoPlayer.vue
+++ src/components/Player/VideoPlayer.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import TimeControl from './TimeControl.vue'
 import VolumeControl from './VolumeControl.vue'
+
+const video = ref()
+const player = ref()
+const videoDuratiion = ref<number>(0)
+const videoCurrentTime = ref<number>(0)
+const setVideoData = () => {
+  if (video.value.readyState) {
+    videoDuratiion.value = video.value.duration
+  }
+}
+const progress = () => {
+  videoCurrentTime.value = video.value.currentTime
+}
+
+const onTimeChange = (value: number) => {
+  video.value.currentTime = value
+}
 </script>
 <template>
   <div class="player" ref="player">
@@ -12,8 +30,15 @@ import VolumeControl from './VolumeControl.vue'
       class="video-screen"
       src="../../assets/video.mp4"
       controls
+      @loadedmetadata="setVideoData"
+      @progress="progress"
     />
-    <time-control />
+    <time-control
+      :video-duratiion="videoDuratiion"
+      :current-video-position="videoCurrentTime"
+      :is-active="true"
+      @on-time-change="onTimeChange"
+    />
     <volume-control />
   </div>
 </template>
@@ -25,7 +50,7 @@ import VolumeControl from './VolumeControl.vue'
   @apply aspect-video shadow-sm block w-full h-full cursor-pointer bg-movie-black;
 }
 
-::-webkit-media-controls {
+/* ::-webkit-media-controls {
   display: none;
-}
+} */
 </style>
